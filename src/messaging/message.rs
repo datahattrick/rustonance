54adb4157c2fecaa6e14ae83ce9e43c643ded6cc
Heath <datahattrick@gmail.com>
2024-08-09 14:30:32 +1200

    use tracing error

--- src/messaging/message.rs
+++ src/messaging/message.rs
@@ -1,9 +1,9 @@
-
+use tracing::error;
 
 /// Checks that a message successfully sent; if not, then logs why to stdout.
 pub fn check_msg<T>(result: serenity::Result<T>) {
     if let Err(why) = result {
-        println!("Error sending message: {:?}", why);
+        error!("Error sending message: {:?}", why);
     }
 }
 
