60f7c9a9ed249735caa60e6cad7e6c99dc74bdda
Heath <datahattrick@gmail.com>
2024-08-09 14:42:18 +1200

    Allow for multi word searches

--- src/commands/play.rs
+++ src/commands/play.rs
@@ -16,22 +16,33 @@ pub enum QueryType {
 }
 
 #[poise::command(prefix_command, guild_only)]
-pub async fn play(ctx: Context<'_>, url: String) ->  Result<(), Error> {
+pub async fn play(ctx: Context<'_>, url: Vec<String>) ->  Result<(), Error> {
     let guild_id = ctx.guild_id().unwrap();
 
     join_channel(ctx).await?;
 
+    let query = url.join(" ");
+
+    check_msg(ctx.say(format!("Grabbing {}", query)).await);
     if let Some(handler_lock) = ctx.data().songbird.get(guild_id) {
         let mut handler = handler_lock.lock().await;
     
-        let track = YouTube::query(ctx, url.to_string()).await.unwrap();
+        let track = YouTube::query(ctx, query).await.unwrap();
         let _track_info = YouTube::info(track.clone()).await;
 
         handler.enqueue_input(track.into()).await;
+        if handler.queue().len() == 1 {
+            check_msg(
+                ctx.say("Playing".to_string())
+                    .await,
+            );
+        } else {
         check_msg(
             ctx.say(format!("Added song to queue: position {}", handler.queue().len()),)
                 .await,
             );
+        };
+        handler.remove_all_global_events();
     } else {
         check_msg(ctx.say("Not in a voice channel to play in").await);
     }
